aaea415e22b832fc8c5acf2a8e227a0f259af3a5
David Andersen <dta001@ucsd.edu>
2017-06-27 17:44:50 -0700

    initial

---
+++ core/RandomMoverSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Savoa;
+
+namespace Train
+{
+    public class RandomMoverSystem : IteratingEntitySystem
+    {
+      override public void AddedToEngine(Engine engine)
+      {
+        entities = engine.EntitiesFor(new Family(typeof(RandomComponent)));
+      }
+
+      override public void processEntity(Entity entity) {
+
+      }
+    }
+
+    class RandomComponent
+    {
+
+    }
+}
---
+++ tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Savoa;
+
+namespace tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+          Engine e = new Engine();
+        }
+    }
+}
