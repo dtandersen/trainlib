897eff712c81e98d5a241d5b733a051b49f8c754
David Andersen <dta001@ucsd.edu>
2017-06-28 16:45:02 -0700

    world stuff

---
+++ core/command/CreateTrain.cs
@@ -0,0 +1,14 @@
+namespace Trains {
+
+class CreateTrain {
+  private World world;
+
+  public CreateTrain(World world) {
+    this.world = world;
+  }
+
+  public void Execute()
+  {
+    Train train = world.createTrain();
+  }
+}
